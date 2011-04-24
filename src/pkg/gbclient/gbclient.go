0c977134b7d58accac487dfcc83881a350039c6a
paulosuzart <paulosuzart@gmail.com>
2011-04-24 00:54:36 -0300

    gofmt

--- src/pkg/gbclient/gbclient.go
+++ src/pkg/gbclient/gbclient.go
@@ -2,27 +2,27 @@ package gbclient
 
 
 import (
-		"http"
-		"os"
-		"log"
-		)
+	"http"
+	"os"
+	"log"
+)
 
 
 type HTTPClient struct {
 	Addr, Method, User, Password string
-	basicAuth bool
-	client *http.Client
+	basicAuth                    bool
+	client                       *http.Client
 }
 
 func NewHTTPClient(addr, method string) (c *HTTPClient) {
 	c = &HTTPClient{
-					Addr : addr, 
-					Method : method, 
-					User : "", 
-					Password : "", 
-					basicAuth : false, 
-					client : new (http.Client),
-					}
+		Addr:      addr,
+		Method:    method,
+		User:      "",
+		Password:  "",
+		basicAuth: false,
+		client:    new(http.Client),
+	}
 	return
 }
 
@@ -34,7 +34,7 @@ func (c *HTTPClient) Auth(usr, passwd string) {
 
 
 func (c *HTTPClient) DoRequest() (response *http.Response, err os.Error) {
-	
+
 	response, _, err = c.client.Get(c.Addr)
 
 	if err != nil {
@@ -43,22 +43,22 @@ func (c *HTTPClient) DoRequest() (response *http.Response, err os.Error) {
 	}
 
 	if response.StatusCode == http.StatusUnauthorized {
-		var req *http.Request = new(http.Request) 
+		var req *http.Request = new(http.Request)
 		var h http.Header = map[string][]string{}
 		h.Add("Authorization", "Basic dGVzdGU6b3BlbiB0ZXN0ZQ==")
-		req.Header = h 
+		req.Header = h
 		req.Method = "GET"
 		req.ProtoMajor = 1
 		req.ProtoMinor = 1
-		req.URL, _ = http.ParseURL(c.Addr)		
-		
-		_, err = c.client.Do(req)		
-		
+		req.URL, _ = http.ParseURL(c.Addr)
+
+		_, err = c.client.Do(req)
+
 		if err != nil {
 			log.Println("ERR")
-			return 
+			return
 		}
-		
-	} 
+
+	}
 	return
 }
